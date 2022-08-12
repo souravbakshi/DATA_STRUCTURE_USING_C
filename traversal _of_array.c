b6cbd6c07a58f712eb49ce10cdcc46049183a6d7
SAUARABH BAKSHI <souravbakshi664@gmail.com>
2022-08-12 19:38:06 +0530

    Update traversal _of_array.c

--- traversal _of_array.c
+++ traversal _of_array.c
@@ -1,12 +1,33 @@
 
-// ARRAY TRAVERSAL 
-#include<stdio.h>  
-int main(){      
-int i=0;    
-int marks[5]={20,30,40,50,60};//declaration and initialization of array    
- //traversal of array    
-for(i=0;i<5;i++){      
-printf("%d \n",marks[i]);    
-}    
-return 0;  
+// ARRAY TAVERSAL -USER INPUT
+#include<stdio.h>
+int main()
+{
+int n,i;
+ //int arr[n];
+//printf("Enter the size of array:");
+//scanf("%d",&n);
+int arr[5]={10,20,30,40,50};
+/*printf("Enter the elements of array:");
+for(i = 0; i < n; i++)
+{
+	printf("\n arr[%d] = ", i);
+scanf("%d",&arr[i]);
+}*/
+printf("\n After Inserting array elements are ");
+for(i = 0; i < 5; i++)
+{
+printf(" \n marks[%d]=",arr[i]);
 }
+return 0;
+}
+
+ALGORITHMS 
+===========
+Algorithm for Traversal Of Array
+Step 01: Start
+Step 02: [Initialize counter variable. ] Set i = LB.
+Step 03: Repeat for i = LB to UB.
+Step 04: Apply process to arr[i].
+Step 05: [End of loop. ]
+Step 06: Stop
